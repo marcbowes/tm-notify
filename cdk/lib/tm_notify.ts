7575f2db91cd07a00843a7de65438c2ce584e5ad
Marc Bowes <bowes@amazon.com>
2021-07-28 13:07:43 -0700

    Upload lambda zipfile

--- cdk/lib/tm_notify.ts
+++ cdk/lib/tm_notify.ts
@@ -1,6 +1,7 @@
 import * as core from "@aws-cdk/core";
 import * as s3 from '@aws-cdk/aws-s3'
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as iam from '@aws-cdk/aws-iam'
 
 export class TmNotify extends core.Construct {
     constructor(scope: core.Construct, id: string) {
@@ -8,10 +9,14 @@ export class TmNotify extends core.Construct {
 
         const bucket = new s3.Bucket(this, "TmNotifyAssets");
 
-        const handler = new lambda.Function(this, "TmNotify", {
-            runtime: lambda.Runtime.PROVIDED_AL2,
-            handler: "custom.runtime",
-            code: lambda.Code.fromBucket(bucket, "release/latest.zip")
-        });
+        // This user will be used to publish artifacts from Github actions.
+        const ghUser = new iam.User(this, "TmGithubUser");
+        bucket.grantWrite(ghUser);
+
+        // const handler = new lambda.Function(this, "TmNotify", {
+        //     runtime: lambda.Runtime.PROVIDED_AL2,
+        //     handler: "custom.runtime",
+        //     code: lambda.Code.fromBucket(bucket, "release/latest.zip")
+        // });
     }
 }
